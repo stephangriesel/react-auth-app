a8e31ae0d732c20237dee7763d9eb12eac31ac53
Stephan Griesel <sgriesel@gmail.com>
2018-06-19 20:49:36 +0200

    add login portal

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withAuth } from "@okta/okta-react";
 
-export default () => {
-  return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+export default withAuth(
+  class Home extends Component {
+    state = { authenticated: null };
+
+    checkAuthentication = async () => {
+      const authenticated = await this.props.auth.isAuthenticated();
+      if (authenticated !== this.state.authenticated) {
+        this.setState({ authenticated });
+      }
+    };
+
+    async componentDidMount() {
+      this.checkAuthentication();
+    }
+
+    async componentDidUpdate() {
+      this.checkAuthentication();
+    }
+
+    login = async () => {
+      this.props.auth.login("/");
+    };
+
+    logout = async () => {
+      this.props.auth.logout("/");
+    };
+
+    render() {
+      if (this.state.authenticated === null) return null;
+
+      const mainContent = this.state.authenticated ? (
+        <div>
+          <p className="lead">
+            You are in the members portal, <Link to="/staff">click here</Link>
+          </p>
+          <button className="btn btn-light btn-lg" onClick={this.logout}>
+            Logout
+          </button>
+        </div>
+      ) : (
+        <div>
+          <p className="lead">
+            If you are a member please get your credentials from the administrator
+          </p>
+          <button className="btn btn-dark btn-lg" onClick={this.logout}>
+            Login
+          </button>
+        </div>
+      );
+
+      return (
+        <div className="jumbotron">
+          <h1 className="display-4">Consulitate Members</h1>
+          {mainContent}
+        </div>
+      );
+    }
+  }
+);
